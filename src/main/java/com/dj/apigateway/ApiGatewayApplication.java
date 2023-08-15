e4ef46bd6ff363ed751db11e60fdd7fb14b76792
Darshan Lapani <darshanlapani07@gmail.com>
2023-08-15 11:45:11 +0530

    made api-server to resouce-server

--- src/main/java/com/dj/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/dj/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.dj.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
